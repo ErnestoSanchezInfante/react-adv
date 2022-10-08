0a9ebc7e288061ee785b2e75d8123cf6c3ac6d0a
Ernesto <ernestosanchezinfante@gmail.com>
2022-10-07 19:54:03 -0500

    Rutas anidadas listas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { lazy, LazyExoticComponent } from "react";
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
 //import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 type JSXComponent = () => JSX.Element;
@@ -10,32 +11,26 @@ interface  Route {
     name: string;  //Es el nombre con el que aparecera
 }
 
-//Se define un componente que va a ser cargado bajo demanda
+//Se define un componente que va a ser cargado bajo demanda, los comentarios son para darle un nombre al chunck
 
-const Lazy1 = lazy( () => import(/* webpackChunkName:"LazyPage1" */'../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy( () => import(/* webpackChunkName:"LazyPage2" */'../01-lazyload/pages/LazyPage2'));
-const Lazy3 = lazy( () => import(/* webpackChunkName:"LazyPage3" */'../01-lazyload/pages/LazyPage3'));
+const LazyLayout = lazy( () => import(/* webpackChunkName:"LazyLayout" */'../01-lazyload/layout/LazyLayout'));
+
 
 export const routes: Route[] = [
     {
-        to:'/lazy1',
-        path:'lazy1',
-        Component: Lazy1, //El componente
-        name: 'Lazy-1'  //Es el nombre con el que aparecera
+        path:'/lazyload/*',
+        to:'/lazyload/',
+        Component: LazyLayout, //El componente
+        name: 'LazyLayout - Dash'  //Es el nombre con el que aparecera
     },
 
     {
-        to:'/lazy2',
-        path:'lazy2',
-        Component: Lazy2, //El componente
-        name: 'Lazy-2'  //Es el nombre con el que aparecera
+        to:'/no-lazy',
+        path:'no-lazy',
+        Component: NoLazy, //El componente
+        name: 'No Lazy'  //Es el nombre con el que aparecera
     },
 
-    {
-        to:'/lazy3',
-        path:'lazy3',
-        Component: Lazy3, //El componente
-        name: 'Lazy-3'  //Es el nombre con el que aparecera
-    },
+
 
 ]
